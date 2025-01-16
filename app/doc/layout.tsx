7ff480d7e2da589e4669bba5c8249c03a25337aa
Paulo Borges <victorborges296@hotmail.com>
2025-01-16 19:26:26 -0300

    LiveBlocksProvider import in doc/layout.tsx fixed

--- app/doc/layout.tsx
+++ app/doc/layout.tsx
@@ -1,4 +1,4 @@
-import LiveBlocksProvider from "@/components/LiveblocksProvider";
+import LiveBlocksProvider from "@/components/LiveBlocksProvider";
 
 function PageLayout({ children }: { children: React.ReactNode }) {
    return (
